9ce801a76ca78ebcdb3c12e27b25a4c5a6898fe8
lucahu004 <hu.luca@iisgalvanimi.edu.it>
2023-04-14 09:54:28 +0000

    commit

---
+++ src/app/models/serviceresponse.model.ts
@@ -0,0 +1,4 @@
+import { Team } from "./team.model";
+export class ServiceResponse{
+    teamData: Team[]
+}
---
+++ src/app/team/team.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Team } from '../models/team.model';
+
+@Component({
+  selector: 'app-team',
+  templateUrl: './team.component.html',
+  styleUrls: ['./team.component.css']
+})
+export class TeamComponent {
+}
